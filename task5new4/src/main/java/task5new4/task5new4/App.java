5f594f35b6f753b27b95174628ab040c35fc00f8
lasyadaruri <darurilasya@gmail.com>
2020-03-01 19:10:25 +0530

    commited

---
+++ task5new4/src/main/java/task5new4/task5new4/App.java
@@ -0,0 +1,13 @@
+package task5new4.task5new4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
